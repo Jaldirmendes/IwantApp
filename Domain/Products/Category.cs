3c0ccc2cca47cf85edf2690e1690650597d6def6
Jaldirmendes <mendesjaldir1@gmail.com>
2022-09-01 16:20:15 -0300

    Ajuste in categories validations

--- Domain/Products/Category.cs
+++ Domain/Products/Category.cs
@@ -5,23 +5,35 @@ namespace  Iwant.Domain.Products;
 
 public class Category : Entity
 {
-    public string Name { get; set; }
-    public bool Active { get; set; }
+    public string Name { get; private set; }
+    public bool Active { get; private set; }
 
     public Category(string name, string createdBy, string editedBy)
     {
-        var contract = new Contract<Category>()
-            .IsNotNullOrEmpty(name, "Name")
-            .IsGreaterOrEqualsThan(name, 3, "Name")
-            .IsNotNullOrEmpty(editedBy, "EditedBy")
-            .IsNotNullOrEmpty(createdBy, "CreatedBy");
-        AddNotifications(contract);
-
         Name = name;
         Active = true;
         CreatedBy = createdBy;
         EditedBy = editedBy;
         CreatedOn = DateTime.UtcNow;
         EditedOn = DateTime.UtcNow;
+        Validate();
+    }
+
+    private void Validate()
+    {
+        var contract = new Contract<Category>()
+                    .IsNotNullOrEmpty(Name, "Name")
+                    .IsGreaterOrEqualsThan(Name, 3, "Name")
+                    .IsNotNullOrEmpty(EditedBy, "EditedBy")
+                    .IsNotNullOrEmpty(CreatedBy, "CreatedBy");
+        AddNotifications(contract);
+    }
+
+    public void editInfo(string name, bool active)
+    {
+        Active = active;
+        Name = name;
+
+        Validate();
     }
 }
